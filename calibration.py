27246996f931088fd85a41d8cf42ecf80e539646
Mateus Riva <mriva@ime.usp.br>
2018-08-10 14:25:30 -0300

    Add contiguity guarantee improvement. Test with higher noise ratios.

--- calibration.py
+++ calibration.py
@@ -35,10 +35,10 @@ model_labelmap[body_center[0]:,body_center[1]:,:body_center[2]][model_labelmap[b
 #observation_dummy = generate_dummy(2)
 #observation_dummy = np.random.normal(model_dummy, 20)
 #observation_dummy = deepcopy(model_dummy)
-observation_dummy = generate_fat_salt_and_pepper_noise(model_dummy, radius=7,amount=0.000015, seed=0)
+observation_dummy = generate_fat_salt_and_pepper_noise(model_dummy, radius=7,amount=0.00004, seed=0)
 observation_dummy[np.logical_or(model_labelmap == 0, model_labelmap == 1)] = 0
 #observation_dummy = random_noise(model_dummy, "speckle", seed=10) #amount=0.05)
-# display_volume(observation_dummy, cmap="gray", title="Observation Input")
+display_volume(observation_dummy, cmap="gray", title="Observation Input")
 
 # Step 2: Generating model graph
 # -----------------------
@@ -63,7 +63,7 @@ observed_labelmap = watershed(magnitude, markers=500, compactness=0.001)-1
 # observed_labelmap = skis.slic(observation_dummy, n_segments=500,
                     # compactness=0.0001, multichannel=False, sigma=(5,5,1))
 # display_segments_as_lines(observation_dummy, observed_labelmap, width=1, level=0.5)
-# display_volume(observed_labelmap,cmap=ListedColormap(np.random.rand(255,3)))
+display_volume(observed_labelmap,cmap=ListedColormap(np.random.rand(255,3)))
 #display_overlayed_volume(observation_dummy, observed_labelmap, label_colors=np.random.rand(255,3),width=1,level=0.5)
 
 # Step 4: Generating super-observation graph
@@ -91,7 +91,7 @@ for i, super_vertex in enumerate(super_graph.vertices):
 
 # Step 6: Contiguity guarantee
 # -----------------------
-uncontiguous_regions, uncontiguous_regions_costs = [], []
+# Detecting non-contiguous regions
 for label, model_vertex in enumerate(model_graph.vertices):
     # Get all contiguous regions for this label
     label_regions = np.where(solution==label)
@@ -108,14 +108,59 @@ for label, model_vertex in enumerate(model_graph.vertices):
     model_label_vertex_matrix = np.vstack([model_vertex]*potential_region_super_graph.vertices.shape[0])
     costs = compute_initial_vertex_cost(potential_region_super_graph.vertices, model_label_vertex_matrix, weights=initial_weights)
     actual_region = np.argmin(costs) # Actual region is the one with the lowest cost
+    # Determining which supervertexes compose the actual region
+    correct_vertexes = np.unique(observed_labelmap[potential_region_map==actual_region])
     # Marking other regions for improvement
-    uncontiguous_regions = np.append(uncontiguous_regions, costs[np.arange(len(label_regions))!=actual_region])
-    uncontiguous_regions_costs = np.append(uncontiguous_regions_costs, costs[np.arange(len(costs))!=actual_region])
+    solution[label_regions] = -1
+    solution[correct_vertexes] = label
 
-print(uncontiguous_regions)
+# Running improvement for non-contiguous regions
+joined_labelmap = np.zeros_like(observed_labelmap)
+for label, model_vertex in enumerate(model_graph.vertices):
+    joined_labelmap[np.isin(observed_labelmap, np.where(solution==label))]=label
+for super_vertex_index, super_vertex in sorted(enumerate(solution_costs), key=lambda x: x[1], reverse=True):
+    if solution[super_vertex_index] > -1:
+        continue # Ignore continuous regions, for now
 
-# Step 7: Region Joining
-# -----------------------
+    potential_predictions = set([solution[neighbour] for neighbour in super_adjacency.adj[super_vertex_index].keys()])
+
+    current_cost = float("inf")
+    current_prediction_index = solution[super_vertex_index]
+    print("Modifying supervertex {} (curr: {}, cost: {:.6f})".format(super_vertex_index, current_prediction_index, current_cost))
+
+    # Soft contiguity: potential predictions may only be neighboring labels
+    for potential_prediction_index in potential_predictions:
+        # Skipping same replacements
+        if potential_prediction_index == current_prediction_index: continue
+        if potential_prediction_index == -1: continue
+
+        # Replacing the current prediction with the potential
+        working_labelmap = deepcopy(joined_labelmap)
+        working_labelmap[observed_labelmap==super_vertex_index] = potential_prediction_index
+
+        # Updating graph
+        working_graph = build_graph(observation_dummy, working_labelmap, target_vertices=model_graph.vertices.shape[0])
+        working_graph = normalize_graph(working_graph, mean_vertex, std_vertex, mean_edge, std_edge)
+
+        # Computing costs
+        potential_vertex_costs = compute_vertex_cost(working_graph.vertices, model_graph.vertices, weights=vertex_weights)
+        potential_edge_costs = compute_edge_cost(working_graph.edges, model_graph.edges)
+        potential_cost = graph_weights[0]*np.mean(potential_vertex_costs) + graph_weights[1]*np.mean(potential_edge_costs)
+        print("\tAttempting replace with {}, cost: {:.6f}".format(potential_prediction_index, potential_cost))
+        # Improving if better
+        if potential_cost < current_cost:
+            current_prediction_index = potential_prediction_index
+            current_vertex_costs = potential_vertex_costs
+            current_edge_costs = potential_edge_costs
+            current_cost = potential_cost
+
+    print("\t* Replaced with {}".format(current_prediction_index))
+
+    solution[super_vertex_index] = current_prediction_index
+
+# End of an epoch, rebuilding the joined graph
+print("End of contiguity guarantee")
+#print("End of epoch #{}: solution = {}".format(epoch,solution))
 joined_labelmap = np.zeros_like(observed_labelmap)
 for label, model_vertex in enumerate(model_graph.vertices):
     joined_labelmap[np.isin(observed_labelmap, np.where(solution==label))]=label
@@ -123,9 +168,17 @@ observation_graph = build_graph(observation_dummy, joined_labelmap, target_verti
 observation_graph = normalize_graph(observation_graph, mean_vertex, std_vertex, mean_edge, std_edge)
 vertex_costs = compute_vertex_cost(observation_graph.vertices, model_graph.vertices, weights=vertex_weights)
 edge_costs = compute_edge_cost(observation_graph.edges, model_graph.edges, weights=edge_weights)
-print("Joined Initial Solution (Costs: {:.3f},{:.3f})".format(np.mean(vertex_costs),np.mean(edge_costs)))
-display_volume(joined_labelmap, cmap=color_map, title="Joined Initial Solution (Costs: {:.3f},{:.3f})".format(np.mean(vertex_costs),np.mean(edge_costs)))
+print("Contiguous Solution (Costs: {:.3f},{:.3f})".format(np.mean(vertex_costs),np.mean(edge_costs)))
 print("Observation:",represent_srg(observation_graph, class_names=class_names))
+
+display_volume(joined_labelmap, cmap=color_map, title="Contiguous Solution (Costs: {:.3f},{:.3f})".format(np.mean(vertex_costs),np.mean(edge_costs)))
+
+# Step 7: Region Joining TODO: add above?
+# -----------------------
+# joined_labelmap = np.zeros_like(observed_labelmap)
+# for label, model_vertex in enumerate(model_graph.vertices):
+#     joined_labelmap[np.isin(observed_labelmap, np.where(solution==label))]=label
+
 
 # Step 8: Improvement
 # -----------------------
@@ -135,17 +188,23 @@ for epoch in range(total_epochs):
     # attempting to improve each vertex, starting from the most expensive
     for super_vertex_index, _ in sorted(enumerate(solution_costs), key=lambda x: x[1], reverse=True)[:improvement_cutoff]:
         current_prediction_index = solution[super_vertex_index]
+
+        potential_predictions = set([solution[neighbour] for neighbour in super_adjacency.adj[super_vertex_index].keys()])
+        if len(potential_predictions) == 1 and next(iter(potential_predictions)) == current_prediction_index:
+            print("Supervertex {} (curr: {}) has no distinct neighbours".format(super_vertex_index, current_prediction_index))
+            solution_costs[super_vertex_index] = 0
+            continue
+
         current_vertex_costs = compute_vertex_cost(observation_graph.vertices, model_graph.vertices, weights=vertex_weights)
         current_edge_costs = compute_edge_cost(observation_graph.edges, model_graph.edges, weights=edge_weights)
         current_cost = graph_weights[0]*np.mean(current_vertex_costs) + graph_weights[1]*np.mean(current_edge_costs)
-
         print("Modifying supervertex {} (curr: {}, cost: {:.6f})".format(super_vertex_index, current_prediction_index, current_cost))
 
         # Soft contiguity: potential predictions may only be neighboring labels
-        potential_predictions = set([solution[neighbour] for neighbour in super_adjacency.adj[super_vertex_index].keys()])
         for potential_prediction_index in potential_predictions:
             # Skipping same replacements
             if potential_prediction_index == current_prediction_index: continue
+            if potential_prediction_index == -1: continue
 
             # Replacing the current prediction with the potential
             working_labelmap = deepcopy(joined_labelmap)
@@ -179,8 +238,8 @@ for epoch in range(total_epochs):
     print("End of epoch #{}".format(epoch))
     #print("End of epoch #{}: solution = {}".format(epoch,solution))
     joined_labelmap = np.zeros_like(observed_labelmap)
-    for element, prediction in enumerate(solution):
-        joined_labelmap[observed_labelmap==element]=prediction
+    for label, model_vertex in enumerate(model_graph.vertices):
+        joined_labelmap[np.isin(observed_labelmap, np.where(solution==label))]=label
     observation_graph = build_graph(observation_dummy, joined_labelmap, target_vertices=model_graph.vertices.shape[0])
     observation_graph = normalize_graph(observation_graph, mean_vertex, std_vertex, mean_edge, std_edge)
     vertex_costs = compute_vertex_cost(observation_graph.vertices, model_graph.vertices, weights=vertex_weights)
