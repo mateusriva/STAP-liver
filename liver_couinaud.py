bef76961ff61b79a211839e2e8a2a92362aa1043
Mateus Riva <mriva@ime.usp.br>
2018-08-22 15:18:10 -0300

    Add liver_couinaud segmentation. Segment predicted liver.
    Create liver_couinaud segmentation script, which uses a liver bounding box (outputted from liver_full).
    Run experiments with the bounding box received from liver_full.
    
    Signed-off-by: Mateus Riva <mriva@ime.usp.br>

--- liver_couinaud.py
+++ liver_couinaud.py
@@ -1,49 +1,51 @@
-"""Full Liver segmentation module for the SRG.
+"""Extracts Couinaud divisions of a previously-segmented liver.
 
-This module contains specific configurations
-for the SRG, in order to make it segment livers.
+This takes as input a cut of a MRI containing only the liver and bounding box.
 
-Authors:
- * Mateus Riva (mriva@ime.usp.br)
+Author
+------
+* Mateus Riva (mriva@ime.usp.br)
 """
 
-from liver_full_functions import *
+from liver_couinaud_functions import *
 
-initial_weights = (1,1,1,1)
-vertex_weights = (1,1,1,1,10)
+initial_weights = (2,2,0.5,1)
+vertex_weights = (1,1,0.5,1,10)
 edge_weights = (1,1,1,1,1)
-graph_weights = (1,1)
+graph_weights = (1,4)
+
+observation_bounding_box = ([121,  62,   0],[354, 339,  41])
 
 #if __name__ == '__main__':
 # Step 1: Loading data
 # -----------------------
 model_patient = Patient.build_from_folder("data/4")
 model_volume, model_labelmap = model_patient.volumes['t2'], model_patient.labelmaps['t2']
-# Reconfiguring model_labelmap with extra backgrounds and unified liver
-model_labelmap.data += 1 # Adding space for the automatic "body" label
-model_labelmap.data[np.logical_and(model_volume.data < 10, model_labelmap.data == 1)] = 0 # automatic body
-model_labelmap.data += 1 # Adding space for the split background
-model_labelmap.data[:model_labelmap.data.shape[1]//2,:,:][model_labelmap.data[:model_labelmap.data.shape[1]//2,:,:] == 1] = 0 # splitting background
-model_labelmap.data[model_labelmap.data == 3] = 2 # vena cava is body
-model_labelmap.data[model_labelmap.data >= 4] = 3 # portal, hepatic veins are 'liver'
+
+# Observation comes from the bounding box given as input
+observation_volume = deepcopy(model_volume)
+observation_volume.data = observation_volume.data[observation_bounding_box[0][0]:observation_bounding_box[1][0],observation_bounding_box[0][1]:observation_bounding_box[1][1],observation_bounding_box[0][2]:observation_bounding_box[1][2]]
+display_volume(observation_volume.data,cmap='gray')
+
+# The model utilizes the precise bounding box of the true liver
+model_bounding_box = (np.argwhere(model_labelmap.data >= 6).min(axis=0),np.argwhere(model_labelmap.data >= 6).max(axis=0))
+model_volume.data = model_volume.data[model_bounding_box[0][0]:model_bounding_box[1][0],model_bounding_box[0][1]:model_bounding_box[1][1],model_bounding_box[0][2]:model_bounding_box[1][2]]
+model_labelmap.data = model_labelmap.data[model_bounding_box[0][0]:model_bounding_box[1][0],model_bounding_box[0][1]:model_bounding_box[1][1],model_bounding_box[0][2]:model_bounding_box[1][2]]
+
 # getting center of body
 body_center = [int(x) for x in measure_center_of_mass(np.ones_like(model_labelmap.data), labels=model_labelmap.data, index=range(4))[2]]
 model_labelmap.data = model_labelmap.data + 7 # adding space for the body divisions
-model_labelmap.data[model_labelmap.data == 7] = 0
-model_labelmap.data[model_labelmap.data == 8] = 1
 # Splitting the body into 8 cubes, based on centroid
-model_labelmap.data[:body_center[0],:body_center[1],:body_center[2]][model_labelmap.data[:body_center[0],:body_center[1],:body_center[2]] == 9] = 2
-model_labelmap.data[:body_center[0],:body_center[1],body_center[2]:][model_labelmap.data[:body_center[0],:body_center[1],body_center[2]:] == 9] = 3
-model_labelmap.data[:body_center[0],body_center[1]:,:body_center[2]][model_labelmap.data[:body_center[0],body_center[1]:,:body_center[2]] == 9] = 4
-model_labelmap.data[:body_center[0],body_center[1]:,body_center[2]:][model_labelmap.data[:body_center[0],body_center[1]:,body_center[2]:] == 9] = 5
-model_labelmap.data[body_center[0]:,:body_center[1],:body_center[2]][model_labelmap.data[body_center[0]:,:body_center[1],:body_center[2]] == 9] = 6
-model_labelmap.data[body_center[0]:,:body_center[1],body_center[2]:][model_labelmap.data[body_center[0]:,:body_center[1],body_center[2]:] == 9] = 7
-model_labelmap.data[body_center[0]:,body_center[1]:,:body_center[2]][model_labelmap.data[body_center[0]:,body_center[1]:,:body_center[2]] == 9] = 8
+model_labelmap.data[:body_center[0],:body_center[1],:body_center[2]][model_labelmap.data[:body_center[0],:body_center[1],:body_center[2]] == 7] = 0
+model_labelmap.data[:body_center[0],:body_center[1],body_center[2]:][model_labelmap.data[:body_center[0],:body_center[1],body_center[2]:] == 7] = 1
+model_labelmap.data[:body_center[0],body_center[1]:,:body_center[2]][model_labelmap.data[:body_center[0],body_center[1]:,:body_center[2]] == 7] = 2
+model_labelmap.data[:body_center[0],body_center[1]:,body_center[2]:][model_labelmap.data[:body_center[0],body_center[1]:,body_center[2]:] == 7] = 3
+model_labelmap.data[body_center[0]:,:body_center[1],:body_center[2]][model_labelmap.data[body_center[0]:,:body_center[1],:body_center[2]] == 7] = 4
+model_labelmap.data[body_center[0]:,:body_center[1],body_center[2]:][model_labelmap.data[body_center[0]:,:body_center[1],body_center[2]:] == 7] = 5
+model_labelmap.data[body_center[0]:,body_center[1]:,:body_center[2]][model_labelmap.data[body_center[0]:,body_center[1]:,:body_center[2]] == 7] = 6
 
-#display_volume(model_labelmap.data, cmap=class_colors)
-# display_overlayed_volume(model_volume.data, model_labelmap.data, label_colors=[(0,0,0),(0.1,0.1,0.1),(0.40,0.40,0.40),(0.42,0.42,0.42),(0.44,0.44,0.44),(0.46,0.46,0.46),(0.48,0.48,0.48),(0.50,0.50,0.50),(0.52,0.52,0.52),(0.54,0.54,0.54),(1,0,0),(0,1,0)], title="Model")
-
-observation_volume = deepcopy(model_volume)
+# display_volume(model_labelmap.data, cmap=class_colors)
+# display_overlayed_volume(model_volume.data, model_labelmap.data, label_colors=class_colors.colors, title="Model")
 
 # Step 2: Generating model graph
 # -----------------------
@@ -56,13 +58,15 @@ print("Model:",represent_srg(model_graph, class_names=class_names))
 # Applying gradient
 smoothed = ndi.gaussian_filter(observation_volume.data, (3,3,1))
 smoothed = smoothed/np.max(smoothed) # normalization for magnitude
-#display_volume(smoothed, cmap="gray")
+# display_volume(smoothed, cmap="gray")
 magnitude = np.sqrt(ndi.filters.sobel(smoothed, axis=0)**2 + ndi.filters.sobel(smoothed, axis=1)**2 + ndi.filters.sobel(smoothed, axis=2)**2)
-#display_volume(magnitude, cmap="gray", title="Magnitude")
-observed_labelmap_data = watershed(magnitude, markers=500, compactness=0.001)-1
+# display_volume(magnitude, cmap="gray", title="Magnitude")
+observed_labelmap_data = watershed(magnitude, markers=1000, compactness=0.01)-1
+# observed_labelmap_data = skis.slic(observation_volume.data, n_segments=500,
+#                     compactness=0.0001, multichannel=False, sigma=(5,5,1))
 display_segments_as_lines(observation_volume.data, observed_labelmap_data)
-#display_volume(observed_labelmap_data,cmap=ListedColormap(np.random.rand(255,3)))
-#display_overlayed_volume(observation_volume.data, observed_labelmap_data, label_colors=np.random.rand(255,3),width=1,level=0.5)
+display_volume(observed_labelmap_data,cmap=ListedColormap(np.random.rand(255,3)))
+# display_overlayed_volume(observation_volume.data, observed_labelmap_data, label_colors=np.random.rand(255,3),width=1,level=0.5)
 
 # Step 4: Generating super-observation graph
 # -----------------------
@@ -78,12 +82,27 @@ solution_costs = np.empty_like(solution)
 for i, super_vertex in enumerate(super_graph.vertices):
     # Computing cost to all model vertices
     super_vertex_matrix = np.vstack([super_vertex]*model_graph.vertices.shape[0])
-    costs = compute_initial_vertex_cost(super_vertex_matrix, model_graph.vertices, weights=(1,1,1,1))
+    costs = compute_initial_vertex_cost(super_vertex_matrix, model_graph.vertices, weights=initial_weights)
     solution[i] = np.argmin(costs)
     solution_costs[i] = np.min(costs)
 # print("Initial solution:")
 # for i, prediction in enumerate(solution):
 #     print("\t{}: {}".format(i, prediction))
+joined_labelmap_data = np.zeros_like(observed_labelmap_data)
+for label, model_vertex in enumerate(model_graph.vertices):
+    joined_labelmap_data[np.isin(observed_labelmap_data, np.where(solution==label))]=label
+observation_graph = build_graph(observation_volume.data, joined_labelmap_data, target_vertices=model_graph.vertices.shape[0])
+observation_graph = normalize_graph(observation_graph, mean_vertex, std_vertex, mean_edge, std_edge)
+vertex_costs = compute_vertex_cost(observation_graph.vertices, model_graph.vertices, weights=vertex_weights)
+edge_costs = compute_edge_cost(observation_graph.edges, model_graph.edges, weights=edge_weights)
+# dice = (2. * np.logical_and(joined_labelmap_data==10, model_labelmap.data == 10)).sum()/((joined_labelmap_data==10).sum() + (model_labelmap.data == 10).sum())
+dice = 0
+print("Initial Solution (Costs: {:.3f},{:.3f}), Dice: {:.4f}".format(np.mean(vertex_costs),np.mean(edge_costs), dice))
+print("Observation:",represent_srg(observation_graph, class_names=class_names))
+
+display_volume(joined_labelmap_data, cmap=class_colors, title="Initial Solution (Costs: {:.3f},{:.3f})".format(np.mean(vertex_costs),np.mean(edge_costs)))
+
+
 
 # Step 6: Contiguity guarantee
 # -----------------------
@@ -167,26 +186,12 @@ observation_graph = build_graph(observation_volume.data, joined_labelmap_data, t
 observation_graph = normalize_graph(observation_graph, mean_vertex, std_vertex, mean_edge, std_edge)
 vertex_costs = compute_vertex_cost(observation_graph.vertices, model_graph.vertices, weights=vertex_weights)
 edge_costs = compute_edge_cost(observation_graph.edges, model_graph.edges, weights=edge_weights)
-dice = (2. * np.logical_and(joined_labelmap_data==10, model_labelmap.data == 10)).sum()/((joined_labelmap_data==10).sum() + (model_labelmap.data == 10).sum())
+# dice = (2. * np.logical_and(joined_labelmap_data==10, model_labelmap.data == 10)).sum()/((joined_labelmap_data==10).sum() + (model_labelmap.data == 10).sum())
+dice = 0
 print("Contiguous Solution (Costs: {:.3f},{:.3f}), Dice: {:.4f}".format(np.mean(vertex_costs),np.mean(edge_costs), dice))
 print("Observation:",represent_srg(observation_graph, class_names=class_names))
 
 display_volume(joined_labelmap_data, cmap=class_colors, title="Contiguous Solution (Costs: {:.3f},{:.3f})".format(np.mean(vertex_costs),np.mean(edge_costs)))
-
-
-# Step 6: Region Joining
-# -----------------------
-joined_labelmap_data = np.zeros_like(observed_labelmap_data)
-for element, prediction in enumerate(solution):
-    joined_labelmap_data[observed_labelmap_data==element]=prediction
-observation_graph = build_graph(observation_volume.data, joined_labelmap_data, target_vertices=model_graph.vertices.shape[0])
-observation_graph = normalize_graph(observation_graph, mean_vertex, std_vertex, mean_edge, std_edge)
-vertex_costs = compute_vertex_cost(observation_graph.vertices, model_graph.vertices, weights=vertex_weights)
-edge_costs = compute_edge_cost(observation_graph.edges, model_graph.edges, weights=edge_weights)
-dice = (2. * np.logical_and(joined_labelmap_data==10, model_labelmap.data == 10)).sum()/((joined_labelmap_data==10).sum() + (model_labelmap.data == 10).sum())
-print("Joined Initial Solution (Costs: {:.3f},{:.3f}), Dice: {:.4f}".format(np.mean(vertex_costs),np.mean(edge_costs), dice))
-display_volume(joined_labelmap_data, cmap=class_colors, title="Joined Initial Solution (Costs: {:.3f},{:.3f})".format(np.mean(vertex_costs),np.mean(edge_costs)))
-print("Observation:",represent_srg(observation_graph, class_names=class_names))
 
 # Step 7: Improvement
 # -----------------------
@@ -244,20 +249,13 @@ for epoch in range(total_epochs):
     observation_graph = normalize_graph(observation_graph, mean_vertex, std_vertex, mean_edge, std_edge)
     vertex_costs = compute_vertex_cost(observation_graph.vertices, model_graph.vertices, weights=vertex_weights)
     edge_costs = compute_edge_cost(observation_graph.edges, model_graph.edges, weights=edge_weights)
-    dice = (2. * np.logical_and(joined_labelmap_data==10, model_labelmap.data == 10)).sum()/((joined_labelmap_data==10).sum() + (model_labelmap.data == 10).sum())
+    # dice = (2. * np.logical_and(joined_labelmap_data==10, model_labelmap.data == 10)).sum()/((joined_labelmap_data==10).sum() + (model_labelmap.data == 10).sum())
+    dice = 0
     print("Epoch {} Solution (Costs: {:.3f},{:.3f}), Dice: {:.4f}".format(epoch, np.mean(vertex_costs),np.mean(edge_costs), dice))
     #display_volume(joined_labelmap_data, cmap=class_colors, title="Epoch {} Solution (Costs: {:.3f},{:.3f})".format(epoch, np.mean(vertex_costs),np.mean(edge_costs)))
     print("Observation:",represent_srg(observation_graph, class_names=class_names))
 
-dice = (2. * np.logical_and(joined_labelmap_data==10, model_labelmap.data == 10)).sum()/((joined_labelmap_data==10).sum() + (model_labelmap.data == 10).sum())
+# dice = (2. * np.logical_and(joined_labelmap_data==10, model_labelmap.data == 10)).sum()/((joined_labelmap_data==10).sum() + (model_labelmap.data == 10).sum())
+dice=0
 print("Epoch {} Solution (Costs: {:.3f},{:.3f}), Dice: {:.4f}".format(epoch, np.mean(vertex_costs),np.mean(edge_costs), dice))
 display_volume(joined_labelmap_data, cmap=class_colors, title="Epoch {} Solution (Costs: {:.3f},{:.3f})".format(epoch, np.mean(vertex_costs),np.mean(edge_costs)))
-
-# Step 8: Outputting bounding box of liver
-# -----------------------
-liver_points = np.argwhere(joined_labelmap_data == 10)
-bounding_box = (liver_points.min(axis=0),liver_points.max(axis=0))
-print("=========================\nFound liver bounding box: {}".format(bounding_box))
-
-
-# TODO: histogramas dos atributos
---
+++ liver_couinaud_functions.py
@@ -0,0 +1,133 @@
+import numpy as np
+import math
+from scipy.ndimage.measurements import center_of_mass as measure_center_of_mass
+from time import time
+from copy import deepcopy
+from itertools import product
+from skimage.morphology import watershed, h_minima, ball, local_minima
+from skimage.color import rgb2gray, gray2rgb
+import skimage.segmentation as skis
+import scipy.ndimage as ndi
+from matplotlib.colors import ListedColormap
+from skimage.util import random_noise
+from skimage.filters import try_all_threshold, threshold_otsu, rank
+import skimage.future.graph as rag
+
+from srg import SRG
+from patient import Patient, LabelMap, Volume
+from display_utils import display_volume, display_segments_as_lines, display_solution, represent_srg, display_overlayed_volume
+class_colors = ListedColormap([(0.40,0.40,0.40),(0.42,0.42,0.42),(0.44,0.44,0.44),(0.46,0.46,0.46),(0.48,0.48,0.48),(0.50,0.50,0.50),(0.52,0.52,0.52),(0.54,0.54,0.54),(0,0,1),(0,1,1),(0.5,0,1),(0,0.5,1),(0,0,0.5),(1,0,0),(1,0.5,0),(0.5,0.5,0),(0,1,0),(0,0.5,0),(1,0.5,1),(0.5,0,0.5),(1,1,0),(1,0,1)])
+class_names = ["BG 1       ", "BG 2       ", "BG 3       ", "BG 4       ", "BG 5       ", "BG 6       ", "BG 7       ", "BG 8       ", "Vena Cava","Portal Vein","Left H. Vein","Middle H. Vein","Right H. Vein","Segment I","Segment II","Segment III","Segment IVa","Segment IVb","Segment V","Segment VI","Segment VII","Segment VIII"]
+
+
+
+def compute_attributes(volume, labelmap, attribute):
+    """Computes an specific attribute for an entire volume"""
+    if attribute == "centroid":
+        labels = np.unique(labelmap)
+        centroids = measure_center_of_mass(np.ones_like(labelmap), labels=labelmap, index=labels)
+        centroids = np.array(centroids)
+        return centroids
+    elif attribute == "intensity":
+        labels, indexes = np.unique(labelmap, return_inverse=True)
+        intensities = np.empty(len(labels))
+        for i, label in enumerate(labels):
+            intensities[i] = np.mean(volume.flatten()[indexes==i])
+        return intensities
+    elif attribute == "size":
+        labels,voxel_count_per_labels = np.unique(labelmap, return_counts=True)
+        sizes = voxel_count_per_labels
+        return sizes
+    else:
+        raise Exception("{} is not a supported attribute".format(attribute))
+
+def build_graph(volume,labelmap,add_edges=True, target_vertices=None):
+    """Builds a graph from an annotated volume."""
+    # Compute statistical attributes
+    centroids = compute_attributes(volume, labelmap, attribute="centroid")
+    intensities = compute_attributes(volume, labelmap, attribute="intensity")
+    sizes = compute_attributes(volume, labelmap, attribute="size")
+
+    # Assemble statistical attributes as the vertex matrix
+    if target_vertices is None:
+        vertices = np.column_stack([centroids, intensities, sizes])
+    else:
+        vertices = np.empty((target_vertices, 5))
+        actual_labels = np.unique(labelmap)
+        actual_index = 0
+        for i, label in enumerate(range(target_vertices)):
+            if label in actual_labels:
+                vertices[label] = np.append(centroids[actual_index],(intensities[actual_index],sizes[actual_index]))
+                actual_index += 1
+            else:
+                vertices[label] = np.array([math.inf]*5)
+
+    if not add_edges:
+        return SRG(vertices, np.array([]), ["centroid_x", "centroid_y", "centroid_z", "intensity", "size"] ,[])
+    else:
+        # Compute relational attributes
+        centroids = vertices[:,:3]
+        intensities = vertices[:,3]
+        sizes = vertices[:,4]
+        positions = np.repeat(centroids, centroids.shape[0],axis=0) - np.vstack([centroids]*centroids.shape[0])
+        contrasts = abs(np.repeat(intensities, intensities.shape[0],axis=0) - np.hstack([intensities]*intensities.shape[0]))
+        ratios = np.repeat(sizes, sizes.shape[0],axis=0) / np.hstack([sizes]*sizes.shape[0])
+        # Assemble relational attributes as the edges matrix
+        edges = np.column_stack([positions, contrasts, ratios])
+
+        # Initializing and returning the SRG
+        return SRG(vertices, edges, ["centroid_x", "centroid_y", "centroid_z", "intensity", "size"], ["position_x","position_y","position_z","contrast","ratio"])#, "contrast", "ratio"])
+
+def normalize_graph(graph, mean_vertex=None, std_vertex=None, mean_edge=None, std_edge=None):
+    """Normalizes a graph's vertex and edge attributes to a given mean and std.
+
+    If any mean and std are not specified, then they will be computed and returned."""
+    return_values = False
+    if mean_vertex is None or std_vertex is None or mean_edge is None or std_edge is None:
+        return_values = True
+
+    vertices = graph.vertices
+    if mean_vertex is None: mean_vertex = vertices.mean(axis=0)
+    if std_vertex is None: std_vertex = vertices.std(axis=0)
+    std_vertex[std_vertex==0] = 1
+    vertices = (vertices - mean_vertex) / std_vertex
+    graph.vertices = vertices
+
+    if len(graph.edges) > 0:
+        edges = graph.edges
+        if mean_edge is None: mean_edge = edges.mean(axis=0)
+        if std_edge is None: std_edge = edges.std(axis=0)
+        std_edge[std_edge==0] = 1
+        edges = (edges - mean_edge) / std_edge
+        graph.edges = edges
+
+    if return_values:
+        return graph, mean_vertex, std_vertex, mean_edge, std_edge
+    else:
+        return graph
+
+def compute_initial_vertex_cost(vertices1, vertices2, weights=None):
+    """Computes initial vertex cost (ignoring size)"""
+    if weights is None:
+        weights = np.ones(vertices1.shape[1]-1)
+    weights = np.array(weights)/sum(weights)
+    costs = np.linalg.norm(weights*(vertices1[:,:-1]-vertices2[:,:-1]), axis=-1)
+    return costs
+
+
+def compute_vertex_cost(vertices1, vertices2, weights=None):
+    """Computes initial vertex cost (ignoring size)"""
+    if weights is None:
+        weights = np.ones(vertices1.shape[1])
+    weights = np.array(weights)/sum(weights)
+    costs = np.linalg.norm(weights*(vertices1-vertices2), axis=-1)
+    return costs
+
+
+def compute_edge_cost(edges1, edges2, weights=None):
+    """Computes edge cost (using vector dissimilarity)"""
+    if weights is None:
+        weights = np.ones(edges1.shape[1])
+    weights = np.array(weights)/sum(weights)
+    costs = np.linalg.norm(weights*(edges1-edges2), axis=-1)
+    return costs
