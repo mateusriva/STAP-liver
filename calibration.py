15390c988fabbc0e21eeab7d916c09851a727808
Mateus Riva <mriva@ime.usp.br>
2018-11-01 20:16:44 -0300

    Calibration and weights update

--- calibration.py
+++ calibration.py
@@ -4,13 +4,13 @@ and for putting them through the segmentation pipeline."""
 
 from calibration_functions import *
 
-initial_weights = (1,1,1,1)
-vertex_weights = (2,1,1,1,1)
+initial_weights = (1,1,1,2)
+vertex_weights = (1,1,1,2,1)
 edge_weights = (1,1,1,1,1)
 graph_weights = (1,1)
 
-w=1
-f=4
+w=4
+f=0
 
 #if __name__ == '__main__':
 # Step 1: Loading data (generating dummies)
@@ -37,8 +37,8 @@ model_labelmap[body_center[0]:,body_center[1]:,:body_center[2]][model_labelmap[b
 # display_volume(model_labelmap, cmap=color_map, title="Model Input")
 #observation_dummy = generate_dummy(2)
 #observation_dummy = np.random.normal(model_dummy, 20)
-#observation_dummy = deepcopy(model_dummy)
-observation_dummy = generate_fat_salt_and_pepper_noise(model_dummy, radius=7,amount=0.00004, seed=0)
+observation_dummy = deepcopy(model_dummy)
+# observation_dummy = generate_fat_salt_and_pepper_noise(model_dummy, radius=7,amount=0.00001*f, seed=0)
 observation_dummy[np.logical_or(model_labelmap == 0, model_labelmap == 1)] = 0
 #observation_dummy = random_noise(model_dummy, "speckle", seed=10) #amount=0.05)
 # display_volume(observation_dummy, cmap="gray", title="Observation Input")
@@ -247,7 +247,7 @@ for epoch in range(total_epochs):
 
 # display_volume(joined_labelmap, cmap=color_map, title="Epoch {} Solution (Costs: {:.3f},{:.3f})".format(epoch, np.mean(vertex_costs),np.mean(edge_costs)))
 
-
+print(solution)
 # saving NRRDs
 import nrrd
 # nrrd.write("calibration_phantom.nrrd", model_dummy)
